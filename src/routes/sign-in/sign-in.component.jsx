4ef8c1a2036d86782669836cb573d6217234764b
MelikeBkr <melikeyaman.bakir@gmail.com>
2022-06-27 12:24:42 +0100

    redirect Google sign in pattern is removed

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,8 @@
-import {useEffect} from 'react';
-import { getRedirectResult } from 'firebase/auth';
 
-import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
 
 
 const SignIn = () => {
-    useEffect(() => {
-        const fetchData = async () => {
-        const response = await getRedirectResult(auth);
-        if(response) {
-            const userDocRef = await createUserDocumentFromAuth(response.user);
-        }
-    }
-fetchData().catch(console.error);}, []);
     const logGoogleUser = async() => {
     const {user} = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -22,7 +12,6 @@ fetchData().catch(console.error);}, []);
        <div>
         <h1>Sign In Page</h1>
         <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-        <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
        </div> 
     );
 };
