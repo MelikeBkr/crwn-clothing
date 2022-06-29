aac13233710a0a998d489374046961d7c5fb6fb1
MelikeBkr <melikeyaman.bakir@gmail.com>
2022-06-29 11:11:51 +0100

    observer pattern is applied

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
@@ -7,6 +7,7 @@ import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+
 
 import './sign-up-form.styles.scss';
 
@@ -38,7 +39,7 @@ const SignUpForm = () => {
         email,
         password
       );
-
+      
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
     } catch (error) {
