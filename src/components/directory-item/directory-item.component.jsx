343cea26ea0f40626d3f3b50edbb1ca2c8ab1e49
MelikeBkr <melikeyaman.bakir@gmail.com>
2022-07-05 10:42:44 +0100

    styled-components pattern transition - part 1

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,20 @@
-import './directory-item.styles.scss'
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from './directory-item.styles';
 
-const DirectoryItem = ({category}) => {
-    const {imageUrl, title} = category;
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Body>
+    </DirectoryItemContainer>
+  );
+};
 
-return (
-    <div className='directory-item-container'>
-    <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
-    <div className='body'>
-    <h2>{title}</h2>
-    <p>Shop Now</p>
-  </div>
-  </div>
-)
-}
-
-export default DirectoryItem
+export default DirectoryItem;
--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -1,36 +1,31 @@
+
+
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const NavigationContainer = styled.div`
-height: 50px;
-width: 100%;
-justify-content: space-between;
-margin-bottom: 25px;
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
 `;
 
 export const LogoContainer = styled(Link)`
-height: 100%;
-width: 70px;
-padding: 25px;
+  height: 100%;
+  width: 70px;
+  padding: 25px;
 `;
 
 export const NavLinks = styled.div`
-position:absolute;
-width: 100px;
-height: 20px;
-border-radius: 100px;
-top: 10px;
-right: 170px;
-margin: 1px;
-box-sizing: border-box;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 export const NavLink = styled(Link)`
-padding: 200px 25px;
-cursor:pointer;
-white-space:nowrap;
-overflow:hidden;
-text-overflow:ellipsis;
+  padding: 10px 15px;
+  cursor: pointer;
 `;
-
-  
